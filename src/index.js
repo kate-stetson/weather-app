d94365a9a589e325c9f0a37c483f7a465cafe775
kate-stetson <beckathryn@gmail.com>
2022-05-07 18:47:14 -0400

    Added Relevant Icon from the API

--- src/index.js
+++ src/index.js
@@ -60,6 +60,15 @@ function showTemperature(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].description);
 }
 
 function displaySearch(city) {
